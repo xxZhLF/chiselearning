98dfcf3b11faa6eec95cc3e08574120fb2ece531
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2024-01-22 23:25:10 +0900

    RawModule Test Failure

--- 0x0001_BasicChisel/1st_ProjBuildUP/Connector/src/main/Connector.scala
+++ 0x0001_BasicChisel/1st_ProjBuildUP/Connector/src/main/Connector.scala
@@ -5,7 +5,8 @@ import chisel3._
 /**
   * RawMdule doesn't generate clock 
   * and reset signals automatically */
-class Connector extends RawModule {
+// class Connector extends RawModule {
+class Connector extends Module {
     val io = IO(new Bundle {
         val  in =  Input(UInt(1.W))
         val out = Output(UInt(1.W))
