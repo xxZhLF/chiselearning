99067c6dc3b8716bd4f828a46edcff2a42b1c7b0
zhou <zhou@axe.bz>
2024-01-22 17:10:40 +0900

    Tried ChiselTest Successfully

--- 0x0001_BasicChisel/1st_ProjBuildUP/Connector/test/src/ConnectorTester.scala
+++ 0x0001_BasicChisel/1st_ProjBuildUP/Connector/test/src/ConnectorTester.scala
@@ -6,7 +6,7 @@ import Connector._
 
 class ConnectorTester extends AnyFlatSpec with ChiselScalatestTester {
     behavior of "Connector"
-    it should "pass" in {
+    it should "Pass" in {
         test(new Connector) { conn =>
             conn.io.in.poke(0.U)
             conn.io.out.expect(0.U)
