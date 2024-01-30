fc9c9570e80dd7c388482f3bd536c3b40fec25a6
zhou <zhou@axe.bz>
2024-01-30 18:18:46 +0900

    Add Comment for Scala Syntax

--- 0x0000_BasicScala/2nd_Grammar/Fibo/src/Fibo.scala
+++ 0x0000_BasicScala/2nd_Grammar/Fibo/src/Fibo.scala
@@ -4,13 +4,29 @@ trait NSIF { /* NonSense InterFace */
 
     var buff: Array[Int]
 
+    /**
+      * Define a function with functional style
+      */
     val printWrapper = (n: Int, bufsz: Int) => {
         print(s"${n}")
         print(if (n == buff(bufsz-1)) "." else ",")
     }
 
-    def Run (begin: Int, end: Int)
-            (HowTo: (Array[Int], Int) => Int): Array[Int] = {
+    /**
+      * Define a function with dual Parameter List (PL).
+      * + First list contains two parameters, the start
+      *   and end of the for loop.
+      * + Second list can only catain one parameter,
+      *   when the parameter type is function value.
+      *   + The function value takes an Array and an
+      *     index as parameters and return an Array.
+      * @param begin Start of for loop
+      * @param end   End of for loop
+      * @param HowTo Callback function
+      * @return buff Array
+      */
+    def Run /* 1st Parameter List */ (begin: Int, end: Int) 
+            /* 2nd Parameter List */ (HowTo: (Array[Int], Int) => Int): Array[Int] = {
         for (i <- Range(begin, end)) {
             if (i < 2) {
                 buff(i) = 1
@@ -27,12 +43,12 @@ class Fibo(stop_at: Int) extends NSIF {
     override var buff: Array[Int] = new Array[Int](stop_at)
 
     def show(): Any = {
-        print("Fibo: ")
-        for (n <- Run(0, stop_at){ 
-            (AnythingIsOk4buff, AnythingIsOk4i) => 
-            AnythingIsOk4buff(AnythingIsOk4i-1) + 
-            AnythingIsOk4buff(AnythingIsOk4i-2)
-        }){
+        print("Fibo: ") /* Call the dual PL */
+        for (n <- Run(0, stop_at){ /* function */
+            (AnythingIsOk4buff, AnythingIsOk4i) => /* buff and index as PL */
+            AnythingIsOk4buff(AnythingIsOk4i-1) +  /*   buff(i-1) */
+            AnythingIsOk4buff(AnythingIsOk4i-2)    /* + buff(i-2) */
+        } /* The function value defined in {} */ ){
             printWrapper(n, stop_at)
         }
         println()
@@ -41,12 +57,12 @@ class Fibo(stop_at: Int) extends NSIF {
 
 object Main { 
     def main(args: Array[String]): Unit = {
-        val fibo = new Fibo(23){
+        val fibo = new Fibo(23){ 
             val useless = "Only Valid Within THIS Object!" 
             def testFun() = {
                 println(useless)
             }
-        }
+        } /* Expend member and method of Fibo Class by {} */
         fibo.show()
         fibo.testFun()
     }
