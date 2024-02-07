200f4632fe644b80284d4b11315b3b8c1770249f
zhou <zhou@axe.bz>
2024-02-07 17:36:38 +0900

    Add Testcase

--- 0x0002_FIFO/FIFO/test/src/FIFOtester.scala
+++ 0x0002_FIFO/FIFO/test/src/FIFOtester.scala
@@ -35,5 +35,25 @@ class CounterTester extends AnyFlatSpec with ChiselScalatestTester {
                 dut.clock.step(2)
             }
         }
+        test(new FIFO(16, 4)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+            val base = 8
+            for (i <- 0 to 16){
+                if (dut.io.enq.isFull.peek().litValue == 1){
+                    dut.io.enq.writing.poke(false.B)
+                    println(f"[${i}%2d] FIFO Full.")
+                } else {
+                    dut.io.enq.writing.poke(true.B)
+                    dut.io.enq.content.poke((base+i).U)
+                }
+                dut.clock.step(2)
+                if (dut.io.deq.isEmpty.peek().litValue == 1){
+                    dut.io.deq.reading.poke(false.B)
+                    println(f"[${i}%2d] FIFO Empty.")
+                } else {
+                    dut.io.deq.reading.poke(true.B)
+                    println(f"[${i}%2d] ReaderIO ${dut.io.deq.content.peek().litValue}%2d")
+                }
+            }
+        }
     }
 }
