a25e1304c50eb00b22d890d8bfda24d1755aaacf
zhou <zhou@axe.bz>
2024-02-07 17:22:33 +0900

    Bubble FIFO

---
+++ 0x0002_FIFO/FIFO/src/main/FIFO.scala
@@ -0,0 +1,73 @@
+package FIFO
+
+import chisel3._
+
+class WriterIO(datsz: Int) extends Bundle {
+    val writing =  Input(Bool())
+    val content =  Input(UInt(datsz.W))
+    val isFull  = Output(Bool())
+}
+
+class ReaderIO(datsz: Int) extends Bundle {
+    val reading =  Input(Bool())
+    val content = Output(UInt(datsz.W))
+    val isEmpty = Output(Bool())
+}
+
+class ____FIFO(datsz: Int) extends Module {
+    val io = IO(new Bundle{
+        val enq = new WriterIO(datsz)
+        val deq = new ReaderIO(datsz)
+    })
+
+    object State extends ChiselEnum {
+        val EMPTY, FULL = Value
+    }
+    import State._
+
+    val stat = RegInit(EMPTY)
+    val data = RegInit(0.U(datsz.W))
+
+    when (stat === EMPTY){
+        when (io.enq.writing){
+            stat := FULL
+            data := io.enq.content
+        }.otherwise{
+        }
+    }.elsewhen(stat === FULL){
+        when (io.deq.reading){
+            stat := EMPTY
+            data := 0.U
+        }.otherwise{
+        }
+    }.otherwise{
+    }
+
+    io.enq.isFull  := stat === FULL
+    io.deq.isEmpty := stat === EMPTY
+    io.deq.content := data
+}
+
+class FIFO(datsz: Int, depth: Int) extends Module{
+    val io = IO(new Bundle{
+        val enq = new WriterIO(datsz)
+        val deq = new ReaderIO(datsz)
+    })
+
+    val buff = Array.fill(depth) (
+        Module(new ____FIFO(datsz))
+    )
+
+    for (i <- 1 until depth){
+        buff(i).io.enq.content   :=  buff(i-1).io.deq.content
+        buff(i).io.enq.writing   := ~buff(i-1).io.deq.isEmpty
+        buff(i-1).io.deq.reading := ~buff(i).io.enq.isFull 
+    }
+
+    io.enq <> buff(0).io.enq
+    io.deq <> buff(depth-1).io.deq
+}
+
+object Main extends App {
+    circt.stage.ChiselStage.emitSystemVerilogFile(new FIFO(16, 4), Array("--target-dir", "HDL"))
+}
---
+++ 0x0002_FIFO/FIFO/test/src/FIFOtester.scala
@@ -0,0 +1,39 @@
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+import FIFO._
+
+class CounterTester extends AnyFlatSpec with ChiselScalatestTester {
+    behavior of "FIFO"
+    it should "Pass" in {
+        test(new FIFO(16, 4)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+            val base = 10
+            for (i <- 0 to 4){
+                if (dut.io.enq.isFull.peek().litValue == 1){
+                    dut.io.enq.writing.poke(false.B)
+                    println("FIFO Full.")
+                } else {
+                    dut.io.enq.writing.poke(true.B)
+                    dut.io.enq.content.poke((base+i).U)
+                    if (dut.io.deq.isEmpty.peek().litValue == 1){
+                        println("FIFO ReaderIO Preparing")
+                    } else {
+                        println("FIFO ReaderIO Ready")
+                    }
+                }
+                dut.clock.step(2)
+            }
+            for (i <- 0 to 4){
+                if (dut.io.deq.isEmpty.peek().litValue == 1){
+                    dut.io.deq.reading.poke(false.B)
+                    println("FIFO Empty.")
+                } else {
+                    dut.io.deq.reading.poke(true.B)
+                    dut.io.deq.content.expect((base+i).U)
+                }
+                dut.clock.step(2)
+            }
+        }
+    }
+}
